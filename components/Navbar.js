00a5148e92992d3089d662006b3a3036baf8e9a0
oh2k1vn <oh2k1vn@gmail.com>
2022-03-23 08:57:37 +0700

    update 23/3

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,98 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
-import { BiMenu, BiChevronDown,BiX } from "react-icons/bi";
+import { BiMenu, BiChevronDown, BiX } from "react-icons/bi";
 
 const Navbar = () => {
+  const menu = [
+    {
+      name: "Trang chủ",
+      route: "/",
+    },
+    {
+      name: "Giới thiệu",
+      route: "/about",
+      item: [
+        {
+          name: "Giới thiệu chung",
+          route: "/#",
+        },
+        {
+          nanme: "Ban chủ nhiệm",
+          route: "/#",
+        },
+        {
+          name: "Giảng viên",
+          route: "/#",
+        },
+      ],
+    },
+    {
+      name: "Đào tạo",
+      route: "https://www.vanlanguni.edu.vn/dao-tao/nganh-dao-tao",
+    },
+    {
+      name: "Tuyển sinh",
+      route: "https://tuyensinh.vanlanguni.edu.vn",
+      item: [
+        {
+          name: "Đại học",
+          route: "/#",
+        },
+        {
+          name: "Cao học",
+          route: "/#",
+        },
+        {
+          name: "CT Đào tạo đặc biệt",
+          route: "/#",
+        },
+      ],
+    },
+    {
+      name: "Tin tức",
+      route: "/blog",
+    },
+    {
+      name: "Liên kết",
+      route: "/#",
+      item: [
+        {
+          name: "Đại học Văn Lang",
+          route: "https://www.vanlanguni.edu.vn",
+        },
+        {
+          name: "Tuyển dụng",
+          route: "/#",
+        },
+        {
+          name: "Fanpage",
+          route: "https://www.facebook.com/cnttvlu/",
+        },
+        {
+          name: "Youtube",
+          route: "https://www.youtube.com/channel/UCA3rs6ydLPgNghvXXF8In4A",
+        },
+        {
+          name: "Online LAB",
+          route: "https://fit.vlu.edu.vn",
+        },
+      ],
+    },
+    // {
+    //   name: "Liên hệ",
+    //   route: "/#",
+    // },
+  ];
+  console.log(menu);
+  const router = useRouter();
+
   return (
     <header>
       <div className="container header">
         <input className="check_hover" type="checkbox" name id="check" hidden />
         <label htmlFor="check" className="check_btn">
-          <BiMenu/>
+          <BiMenu />
         </label>
         <label htmlFor="check" className="overlay" />
         <div className="logo">
@@ -20,14 +104,14 @@ const Navbar = () => {
         </div>
         <div className="menu">
           <label htmlFor="check" className="check_cls">
-            <BiX/>
+            <BiX />
           </label>
-          <li>
+          {/* <li className={router.asPath == "/" ? "active" : ""}>
             <Link href="/">
               <a>Trang chủ</a>
             </Link>
           </li>
-          <li>
+          <li  className={router.asPath == "/about" ? "active" : ""}>
             <Link href="/about">
               <a>
                 Giới thiệu
@@ -35,24 +119,24 @@ const Navbar = () => {
               </a>
             </Link>
             <ul className="menu_item">
-              <li>
-                <Link href="/community.html">
+              <li className={router.asPath == "/community" ? "active" : ""}>
+                <Link href="/community">
                   <a>Giới thiệu chung</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/#" ? "active" : ""}>
                 <Link href="#!">
                   <a>Ban chủ nhiệm</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/#" ? "active" : ""}>
                 <Link href="#">
                   <a>Giảng viên</a>
                 </Link>
               </li>
             </ul>
           </li>
-          <li>
+          <li className={router.asPath == "/#" ? "active" : ""}>
             <Link href="#">
               <a>
                 Đào tạo
@@ -60,34 +144,34 @@ const Navbar = () => {
               </a>
             </Link>
             <ul className="menu_item">
-              <li>
+              <li className={router.asPath == "/#" ? "active" : ""}>
                 <Link href="#">
                   <a>Chương trình đại học</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/#" ? "active" : ""}>
                 <Link href="#">
                   <a>Ngành &amp; Chuyên</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/#" ? "active" : ""}>
                 <Link href="#">
                   <a>Cao học</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/#" ? "active" : ""}>
                 <Link href="#">
                   <a>Chuẩn đầu ra</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/#" ? "active" : ""}>
                 <Link href="#">
                   <a>Đề cương</a>
                 </Link>
               </li>
             </ul>
           </li>
-          <li>
+          <li className={router.asPath == "/#" ? "active" : ""}>
             <Link href="#">
               <a>
                 Tuyển sinh
@@ -95,66 +179,89 @@ const Navbar = () => {
               </a>
             </Link>
             <ul className="menu_item">
-              <li>
+              <li className={router.asPath == "/##" ? "active" : ""}>
                 <Link href="#">
                   <a>Đại học</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/#" ? "active" : ""}>
                 <Link href="#">
                   <a>Cao học</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/#" ? "active" : ""}>
                 <Link href="#">
                   <a>CT đào tạo đặc biệt</a>
                 </Link>
               </li>
             </ul>
           </li>
-          <li>
+          <li className={router.asPath == "/blog" ? "active" : ""}>
             <Link href="/blog">
               <a>Tin tức</a>
             </Link>
           </li>
-          <li>
+          <li className={router.asPath == "/##" ? "active" : ""}>
             <a href="#">
               liên kết
               <i className="bx bx-chevron-down" />
             </a>
             <ul className="menu_item">
-              <li>
+              <li className={router.asPath == "/##" ? "active" : ""}>
                 <Link href="https://www.vanlanguni.edu.vn">
                   <a>Đại học Văn lang</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/##" ? "active" : ""}>
                 <Link href="#!">
                   <a>Tuyển dụng</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/##" ? "active" : ""}>
                 <Link href="https://www.facebook.com/cnttvlu/">
                   <a>Fanpage</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/#" ? "active" : ""}>
                 <Link href="https://www.youtube.com/channel/UCA3rs6ydLPgNghvXXF8In4A">
                   <a>Youtube</a>
                 </Link>
               </li>
-              <li>
+              <li className={router.asPath == "/#" ? "active" : ""}>
                 <Link href="https://fit.vlu.edu.vn">
                   <a>Online LAB</a>
                 </Link>
               </li>
             </ul>
           </li>
-          <li>
+          <li className={router.asPath == "/#" ? "active" : ""}>
             <Link href="#">
               <a>liên hệ</a>
             </Link>
-          </li>
+          </li> */}
+          {menu.map((menu, key) => {
+            return (
+              <li
+                className={router.asPath == menu.route ? "active" : ""}
+                key={key}
+              >
+                <Link href={menu.route}>
+                  {menu.name}
+                  {/* <ul className="menu_item">
+                    {menu.item.map((item, key) => {
+                      return (
+                        <li className={router.asPath == "/" ? "active" : ""}>
+                          <Link href={item.route}>
+                            {item.name}
+                          </Link>
+                        </li>
+                      );
+                    })}
+                  </ul> */}
+                </Link>
+              </li>
+            );
+          })}
         </div>
         <div className="user">
           <div className="user_icon">
--- pages/about.js
+++ pages/about.js
@@ -1,116 +1,76 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import Modal from "../components/Modal/Modal";
 
 export default function about() {
+  const [show, setShow] = useState(false);
+
   return (
-    <>
-      <section id="about">
-        <div className="container">
-          <p className="body_text">Who is building UniWorld Ecosystem?</p>
-          <p className="body_text">Foundation Team</p>
-          <div className="about">
-            <div className="about_item">
-              <Link href="#">
-                <a>
-                  <Image
-                    src="/image/avantar.png"
-                    alt=""
-                    width={389.95}
-                    height={320}
-                    layout="responsive"
-                  />
-                </a>
-              </Link>
-              <div className="about_item_bottom">
-                <Link href="#">
-                  <a className="about_item_bottom-title">Daika Ginza</a>
-                </Link>
-                <p className="about_item_bottom-position">
-                  Founder &amp; Chairman
-                </p>
-                <p className="about_item_bottom-caption">
-                  Serial entrepreneur of various startups - expert in venture
-                  builder. Successfully managed an investment portfolio in real
-                  estate, cryptos.
-                </p>
-                <button>Đọc thêm</button>
-              </div>
-              <div className="about_item_check"></div>
-            </div>
-            <div className="about_item">
-              <Link href="#">
-                <a>
-                  <Image
-                    src="/image/avantar.png"
-                    alt=""
-                    width={389.95}
-                    height={320}
-                    layout="responsive"
-                  />
-                </a>
-              </Link>
-              <div className="about_item_bottom">
-                <Link href="#">
-                  <a className="about_item_bottom-title">Daika Ginza</a>
-                </Link>
-                <p className="about_item_bottom-position">
-                  Founder &amp; Chairman
-                </p>
-                <p className="about_item_bottom-caption">
-                  Serial entrepreneur of various startups - expert in venture
-                  builder. Successfully managed an investment portfolio in real
-                  estate, cryptos.
-                </p>
-                <button>Đọc thêm</button>
-              </div>
-            </div>
-            <div className="about_item">
-              <Link href="#">
-                <a>
-                  <Image
-                    src="/image/avantar.png"
-                    alt=""
-                    width={389.95}
-                    height={320}
-                    layout="responsive"
-                  />
-                </a>
-              </Link>
-              <div className="about_item_bottom">
-                <Link href="#">
-                  <a className="about_item_bottom-title">
-                    Daika Ginza
-                  </a>
-                </Link>
-                <p className="about_item_bottom-position">
-                  Founder &amp; Chairman
-                </p>
-                <p className="about_item_bottom-caption">
-                  Serial entrepreneur of various startups - expert in venture
-                  builder. Successfully managed an investment portfolio in real
-                  estate, cryptos.
-                </p>
-                <button>Đọc thêm</button>
-              </div>
-            </div>
+    <div className="container about">
+      <p className="about_text">Who is building UniWorld Ecosystem?</p>
+      <p className="about_text">Foundation Team</p>
+      <div className="about_content">
+        <div className="about_content_item">
+          <Link href="#">
+            <a>
+              <Image
+                src="/image/avantar.png"
+                alt=""
+                width={389.95}
+                height={320}
+                layout="responsive"
+              />
+            </a>
+          </Link>
+          <div className="about_content_item_bottom">
+            <Link href="#">
+              <a className="about_content_item_bottom-title">Daika Ginza</a>
+            </Link>
+            <p className="about_content_item_bottom-position">
+              Founder &amp; Chairman
+            </p>
+            <p className="about_content_item_bottom-caption">
+              Serial entrepreneur of various startups - expert in venture
+              builder. Successfully managed an investment portfolio in real
+              estate, cryptos.
+            </p>
+            <button onClick={() => setShow(true)}>Đọc thêm</button>
           </div>
-          <div className="block">
-            <div className="block_item">
-              <div className="block_item_text">
-                <h2>Developer team</h2>
-                <p>
-                  The main responsiblity for the development of products in the
-                  Uniworld ecosystem is the Unilab DAO network team.
-                </p>
-                <p>
-                  Unilab was established in 2013, with members from many
-                  countries around the world (Japan, Canada, Hungary, Singapore,
-                  Vietnam...) and operates under the DAO model.
-                </p>
-              </div>
+          <Modal title="Daika Ginza" onClose={() => setShow(false)} show={show}>
+            <Image
+              src="/image/avantar.png"
+              alt=""
+              width={50}
+              height={50}
+              layout="responsive"
+            />
+            <div className="modal-body-text">
+              <h3>Daika Ginza</h3>
+              <p>
+                Serial entrepreneur of various startups - expert in venture
+                builder. Successfully managed an investment portfolio in real
+                estate, cryptos.
+              </p>
             </div>
-            <div className="block_item">
+          </Modal>
+        </div>
+      </div>
+      <div className="home">
+        <div className="block">
+          <div className="block_flex">
+            <div className="block_title">
+              <h2 className="block_title_h2">Developer team</h2>
+              <p className="block_title_note">
+                The main responsiblity for the development of products in the
+                Uniworld ecosystem is the Unilab DAO network team.
+                <br />
+                Unilab was established in 2013, with members from many countries
+                around the world (Japan, Canada, Hungary, Singapore, Vietnam...)
+                and operates under the DAO model.
+              </p>
+            </div>
+            <div className="block_img">
               <Image
                 src="/image/blog1.png"
                 alt=""
@@ -120,23 +80,20 @@ export default function about() {
               />
             </div>
           </div>
-          <div className="block">
-            <div className="block_item">
-              <div className="block_item_text">
-                <h2>Decentralized Community</h2>
-                <p>
-                  Blockchain industry is revolutionary, open source and relies
-                  on the community is continuous intellectual contribution.
-                </p>
-                <p>
-                  We are well aware of the smallness and limitations of
-                  individuals or organizations, so Uniworld is development
-                  philosophy is to combine the initiative of the development
-                  team and inherit and absorb contributions from the community
-                </p>
-              </div>
+          <div className="block_flex">
+            <div className="block_title">
+              <h2 className="block_title_h2">Decentralized Community</h2>
+              <p className="block_title_note">
+                Blockchain industry is revolutionary, open source and relies on
+                the community is continuous intellectual contribution.
+                <br />
+                We are well aware of the smallness and limitations of
+                individuals or organizations, so Uniworld is development
+                philosophy is to combine the initiative of the development team
+                and inherit and absorb contributions from the community
+              </p>
             </div>
-            <div className="block_item">
+            <div className="block_img">
               <Image
                 src="/image/blog1.png"
                 alt=""
@@ -147,7 +104,7 @@ export default function about() {
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </div>
   );
 }
