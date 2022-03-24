1ad0ce6b8a2dedcbaa7843640ee11c76b77b92da
oh2k1vn <oh2k1vn@gmail.com>
2022-03-24 11:17:37 +0700

    demo

---
+++ pages/blogDetail.js
@@ -0,0 +1,388 @@
+import React from "react";
+
+export default function blogDetail() {
+  return (
+    <div className="container">
+      <div className="blogdetail">
+        <img
+          src="https://www.vanlanguni.edu.vn/images/Vlu-Hong-Ngan/thang-ba/vlu-tu%C3%BDensinhnangkhieu/vlu-ky-thi-nang-khieu-san-khau-dien-anh-van-lang-p.gif"
+          alt=""
+          className="w-100"
+        />
+
+        <p className="blogdetail_day">
+          Thứ Bảy, ngày 12/03/2022 16:19 PM (GMT+7)
+        </p>
+
+        <div className="blogdetail_content">
+          <p>
+            <span>
+              <span>
+                Từ ng&agrave;y 20/03/2022 đến hết ng&agrave;y 10/04/2022, Trường
+                Đại học Văn Lang nhận hồ sơ đăng k&yacute; dự thi m&ocirc;n năng
+                khiếu Vẽ, &Acirc;m nhạc v&agrave; S&acirc;n khấu Điện ảnh. Kết
+                quả thi c&aacute;c m&ocirc;n thi được sử dụng để x&eacute;t
+                tuyển v&agrave;o 10 ng&agrave;nh năng khiếu: Kiến tr&uacute;c,
+                Thiết kế Nội thất, Thiết kế C&ocirc;ng nghiệp, Thiết kế Thời
+                trang, Thiết kế Đồ họa, Thiết kế Mỹ thuật số,&nbsp; Piano, Thanh
+                nhạc, Diễn vi&ecirc;n Kịch&nbsp;<span>&ndash;</span>&nbsp;Điện
+                ảnh &ndash; Truyền h&igrave;nh, Đạo diễn Điện ảnh&nbsp;
+                <span>&ndash;</span>&nbsp;Truyền h&igrave;nh.
+              </span>
+            </span>
+          </p>
+          <p>
+            Năm 2022, c&aacute;c m&ocirc;n thi năng khiếu Vẽ, &Acirc;m nhạc,
+            S&acirc;n khấu Điện ảnh của Trường Đại học Văn Lang đều c&oacute; 2
+            phương thức thi để th&iacute; sinh lựa chọn:
+          </p>
+          <ol>
+            <li>Thi tập trung trực tiếp tại Trường Đại học Văn Lang</li>
+            <li>Thi online: gửi b&agrave;i dự thi kết hợp phỏng vấn online</li>
+          </ol>
+          <p>
+            <img
+              src="https://www.vanlanguni.edu.vn/images/Vlu-Hong-Ngan/thang-ba/vlu-tu%C3%BDensinhnangkhieu/vlu-thi-nang-khieu--nam-2021.jpg"
+              alt="vlu thi nang khieu nam 2021"
+            />
+          </p>
+          <p>&nbsp;</p>
+          <p>
+            <span>
+              Th&ocirc;ng tin c&aacute;c m&ocirc;n thi năng khiếu tương ứng với
+              c&aacute;c ng&agrave;nh năng khiếu như sau:
+            </span>
+          </p>
+          <p>
+            <span>
+              <span>
+                <img
+                  src="https://www.vanlanguni.edu.vn/images/Vlu-Hong-Ngan/thang-ba/vlu-tu%C3%BDensinhnangkhieu/TSNK.jpg"
+                  alt="TSNK"
+                />
+              </span>
+            </span>
+          </p>
+          <p>
+            <span>
+              <span>Thời hạn đăng k&yacute;</span>
+            </span>
+          </p>
+          <ul>
+            <li>
+              <span>Đợt 1: 20/3/2022 &ndash; 10/4/2022</span>
+            </li>
+            <li>
+              <span>Đợt 2 (dự kiến): 10/5 &ndash; 30/6/2022</span>
+            </li>
+          </ul>
+          <p>
+            <span>
+              <span>Hồ sơ dự thi</span>
+            </span>
+          </p>
+          <p>
+            <span>
+              (1) Phiếu đăng k&yacute; dự thi theo mẫu của Trường Đại học Văn
+              Lang (
+              <a
+                class="wf_file"
+                href="https://www.vanlanguni.edu.vn/images/VLU_My/hai_khong_hai_muoi/hai_khong_hai_hai/Phi%E1%BA%BFu_%C4%91%C4%83ng_k%C3%BD_d%E1%BB%B1_thi_m%C3%B4n_n%C4%83ng_khi%E1%BA%BFu_-_c%E1%BA%ADp_nh%E1%BA%ADt_-_2202-%C4%91%C3%A3_gi%E1%BA%A3m.pdf"
+              >
+                <span class="wf_file_text">tải tại đ&acirc;y</span>
+              </a>
+              ).
+            </span>
+          </p>
+          <p>
+            <span>
+              (2) 02 tấm h&igrave;nh (4x6) /1 m&ocirc;n thi (ghi r&otilde; họ
+              t&ecirc;n, ng&agrave;y th&aacute;ng năm sinh sau mỗi h&igrave;nh),
+              thời gian chụp h&igrave;nh kh&ocirc;ng qu&aacute; 6 th&aacute;ng
+              so với thời điểm đăng k&yacute; dự thi.
+            </span>
+          </p>
+          <p>
+            <span>
+              (3) CMND/CCCD (bản photocoy c&oacute; c&ocirc;ng chứng).
+            </span>
+          </p>
+          <p>
+            <span>
+              (4) Lệ ph&iacute;: 350.000 đồng/ m&ocirc;n thi/ đợt thi/ phương
+              &aacute;n thi.
+            </span>
+          </p>
+          <p>
+            <span>
+              Th&iacute; sinh c&oacute; thể nộp lệ ph&iacute; theo một trong hai
+              c&aacute;ch:
+            </span>
+          </p>
+          <ul>
+            <li>
+              <span>
+                Nộp trực tiếp tại Văn ph&ograve;ng Tuyển sinh Cơ sở ch&iacute;nh
+                (69/68 Đặng Th&ugrave;y Tr&acirc;m, P.13, Q. B&igrave;nh Thạnh),
+                hoặc Cơ sở 1 (45 Nguyễn Khắc Nhu, P. C&ocirc; Giang, Quận 1,
+                Tp.HCM).
+              </span>
+            </li>
+            <li>
+              <span>
+                Chuyển khoản cho Trường Đại học Văn Lang (STK: 1602201001804,
+                Ng&acirc;n h&agrave;ng Agribank - Chi nh&aacute;nh 3,
+                Tp.HCM).&nbsp;
+              </span>
+              Th&iacute; sinh khi chuyển khoản lệ ph&iacute; cần ghi r&otilde;
+              nội dung chuyển tiền theo cấu tr&uacute;c:&nbsp;
+              <span>
+                <em>
+                  Họ v&agrave; t&ecirc;n &ndash; Số CMND/CCCD &ndash; Số điện
+                  thoại &ndash; Lệ ph&iacute; thi AN/ SKĐA/ Vẽ
+                </em>
+                &nbsp;<em>(</em>
+              </span>
+              <em>
+                th&iacute; sinh thi m&ocirc;n năng khiếu n&agrave;o th&igrave;
+                ghi nội dung chuyển lệ ph&iacute; thi m&ocirc;n năng khiếu
+                đ&oacute;).&nbsp;
+              </em>
+              Trong trường hợp th&iacute; sinh kh&ocirc;ng tham gia kỳ thi,
+              Trường kh&ocirc;ng ho&agrave;n lệ ph&iacute;.
+            </li>
+          </ul>
+          <p>
+            <span>
+              <span>Phương thức đăng k&yacute; dự thi:</span>&nbsp;
+            </span>
+            Th&iacute; sinh c&oacute; thể đăng k&yacute; dự thi năng khiếu theo
+            01 trong 03 c&aacute;ch:
+          </p>
+          <p>
+            <span>
+              (1) Nộp trực tiếp tại Văn ph&ograve;ng Tuyển sinh Trường Đại học
+              Văn Lang
+            </span>
+          </p>
+          <ul>
+            <li>
+              <span>
+                <em>Cơ sở ch&iacute;nh:</em>&nbsp;69/68 Đặng Th&ugrave;y
+                Tr&acirc;m, P.13, Q. B&igrave;nh Thạnh, Tp.HCM
+              </span>
+            </li>
+            <li>
+              <span>
+                <em>Cơ sở 1</em>: 45 Nguyễn Khắc Nhu, P. C&ocirc; Giang, Quận 1,
+                Tp.HCM
+              </span>
+            </li>
+            <li>
+              Tải phiếu đăng k&iacute;:&nbsp;
+              <a href="https://vanlangunivn-my.sharepoint.com/:b:/g/personal/tuyensinh_vanlanguni_edu_vn/EdZRERJr6XVBi4gYpp4b7LMBHkILLYLNG0Jik5ea3A3apw?e=mc6TWe">
+                Tại đ&acirc;y
+              </a>
+            </li>
+          </ul>
+          <p>
+            <span>
+              (2) Gửi hồ sơ đăng k&yacute; qua đường bưu điện về Trường Đại học
+              Văn Lang (
+              <em>45 Nguyễn Khắc Nhu, P. C&ocirc; Giang, Q.1, Tp.HCM</em>).
+            </span>
+          </p>
+          <p>
+            <span>
+              (3) Đăng k&yacute; trực tuyến tại cổng th&ocirc;ng tin của Trường
+              Đại học Văn Lang (
+              <a href="https://thinangkhieu.vanlanguni.edu.vn/">
+                https://thinangkhieu.vanlanguni.edu.vn/
+              </a>
+              )&nbsp;Cổng th&ocirc;ng tin mở cho th&iacute; sinh đăng k&yacute;
+              từ ng&agrave;y 20/3/2022.
+            </span>
+          </p>
+          <p>
+            <span>
+              <span>Phương thức tổ chức thi tuyển v&agrave; thời gian thi</span>
+            </span>
+          </p>
+          <p>
+            <span>
+              <span>Đối với c&aacute;c m&ocirc;n năng khiếu Vẽ</span>
+            </span>
+          </p>
+          <p>
+            <span>
+              Trường Đại học Văn Lang tổ chức thi tuyển theo 02 phương thức với
+              02 đợt thi tuyển như sau:
+            </span>
+          </p>
+          <p>
+            <span>
+              <span>Thi tuyển tập trung</span>&nbsp;tại Cơ sở ch&iacute;nh của
+              Trường Đại học Văn Lang (69/68 Đặng Th&ugrave;y Tr&acirc;m, P.13,
+              Q. B&igrave;nh Thạnh, Tp.HCM)
+            </span>
+          </p>
+          <ul>
+            <li>
+              <span>
+                <span>Đợt 1</span>&nbsp;(dự kiến): 16, 17/4/2022
+              </span>
+            </li>
+            <li>
+              <span>
+                <span>Đợt 2</span>&nbsp;(dự kiến): 23, 24/ 7/2022
+              </span>
+            </li>
+          </ul>
+          <p>
+            <span>
+              <span>Thi tuyển trực tuyến (online)</span>
+            </span>
+          </p>
+          <ul>
+            <li>
+              <span>
+                <span>Đợt 1</span>&nbsp;(dự kiến): 28, 29/4/2022
+              </span>
+            </li>
+            <li>
+              <span>
+                <span>Đợt 2</span>&nbsp;(dự kiến): 30, 31/ 7/2022
+              </span>
+            </li>
+          </ul>
+          <p>
+            <span>
+              <em>
+                Th&iacute; sinh dự thi đợt 1 ho&agrave;n th&agrave;nh đăng
+                k&yacute; hồ sơ v&agrave; nộp lệ ph&iacute; trước ng&agrave;y
+                15/4/20
+              </em>
+              <em>2</em>
+              <em>2</em>.&nbsp;
+              <em>
+                Th&iacute; sinh dự thi đợt 2 ho&agrave;n th&agrave;nh nộp lệ
+                ph&iacute; thi trước ng&agrave;y 30/6/20
+              </em>
+              22.
+            </span>
+          </p>
+          <p>
+            <span>
+              <span>
+                <span>
+                  Đối với c&aacute;c m&ocirc;n năng khiếu &Acirc;m nhạc, năng
+                  khiếu S&acirc;n khấu Điện ảnh
+                </span>
+              </span>
+              <br />
+            </span>
+          </p>
+          <p>
+            <span>
+              Trường Đại học Văn Lang tổ chức thi tuyển theo phương thức thi tập
+              trung hoặc thi trực tuyến; tổ chức linh hoạt nhiều đợt thi trong
+              khoảng thời gian từ th&aacute;ng 4 đến th&aacute;ng 9/2022.
+              Nh&agrave; trường li&ecirc;n hệ, tổ chức cho th&iacute; sinh
+              &ocirc;n luyện v&agrave; thi tuyển dựa tr&ecirc;n hồ sơ đăng
+              k&yacute;.
+            </span>
+          </p>
+          <p>
+            <span>
+              <span>
+                Sử dụng điểm thi năng khiếu của c&aacute;c trường kh&aacute;c
+              </span>
+            </span>
+          </p>
+          <p>
+            <span>
+              Năm 2022, Trường Đại học Văn Lang tiếp tục sử dụng kết quả dự thi
+              c&aacute;c m&ocirc;n năng khiếu của th&iacute; sinh đ&atilde; dự
+              thi tại c&aacute;c trường đại học kh&aacute;c, cụ thể:
+            </span>
+          </p>
+          <p>
+            <span>
+              -&nbsp;
+              <span>
+                <em>M&ocirc;n năng khiếu Vẽ</em>
+              </span>
+              : Trường Đại học Văn Lang nhận điểm từ 06 trường đại học: Trường
+              Đại học Kiến tr&uacute;c Tp. Hồ Ch&iacute; Minh, Trường Đại học Mỹ
+              thuật Tp. Hồ Ch&iacute; Minh, Trường Đại học T&ocirc;n Đức Thắng,
+              Trường Đại học Kiến tr&uacute;c H&agrave; Nội, Trường Đại học Mỹ
+              thuật C&ocirc;ng nghiệp, Trường Đại học Nghệ thuật - Đại học Huế.
+            </span>
+          </p>
+          <p>
+            <span>
+              Th&iacute; sinh nộp bổ sung phiếu điểm thi năng khiếu từ
+              c&aacute;c trường đại học n&agrave;y về Trường Đại học Văn Lang
+              trước ng&agrave;y 31/5/2022 (đợt 1) v&agrave; trước 31/7/2022 (đợt
+              2).
+            </span>
+          </p>
+          <p>
+            <span>
+              -&nbsp;
+              <span>
+                <em>M&ocirc;n năng khiếu &Acirc;m nhạc</em>
+              </span>
+              : Trường Đại học Văn Lang nhận điểm từ 04 trường/ học viện: Học
+              viện &Acirc;m nhạc Quốc gia Việt Nam, Nhạc viện Tp. Hồ Ch&iacute;
+              Minh, Viện &Acirc;m nhạc H&agrave; Nội, Học Viện &acirc;m nhạc
+              Huế.&nbsp;
+            </span>
+            Th&iacute; sinh nộp bổ sung phiếu điểm thi năng khiếu từ c&aacute;c
+            trường đại học n&agrave;y về Trường Đại học Văn Lang trước
+            ng&agrave;y 31/7/2022.
+          </p>
+          <p>
+            <span>
+              -&nbsp;
+              <span>
+                <em>M&ocirc;n năng khiếu S&acirc;n khấu Điện ảnh</em>
+              </span>
+              : Trường Đại học Văn Lang nhận điểm từ Trường Đại học S&acirc;n
+              khấu Điện ảnh Tp.HCM v&agrave; Trường Đại học S&acirc;n khấu Điện
+              ảnh H&agrave; Nội.&nbsp;
+            </span>
+            Th&iacute; sinh nộp bổ sung phiếu điểm thi năng khiếu từ c&aacute;c
+            trường đại học n&agrave;y về Trường Đại học Văn Lang trước
+            ng&agrave;y 31/7/2022.
+          </p>
+          <p>
+            <span class=" wf_caption">
+              <img
+                src="https://www.vanlanguni.edu.vn/images/Vlu-Hong-Ngan/thang-ba/vlu-tu%C3%BDensinhnangkhieu/vlu-ky-thi-nang-khieu-san-khau-dien-anh-van-lang-p.gif"
+                alt="vlu ky thi nang khieu san khau dien anh van lang p"
+                width="1200"
+              />
+              <span>
+                Th&iacute; sinh dự thi năng khiếu S&acirc;n khấu Điện ảnh
+                v&agrave;o Trường Đại học Văn Lang năm 2021
+              </span>
+            </span>
+          </p>
+          <p>
+            <span>
+              <span>Tư vấn &ndash; &ocirc;n luyện</span>
+            </span>
+          </p>
+          <p>
+            Nếu c&oacute; nhu cầu &ocirc;n luyện, phụ huynh, th&iacute; sinh vui
+            l&ograve;ng li&ecirc;n hệ theo số hotline của Khoa Nghệ thuật
+            S&acirc;n khấu Điện ảnh: 0903.858.824 (ThS. Nguyễn Thị Qu&yacute;),
+            Khoa Nghệ thuật Ứng dụng: 0941.572.209 (C&ocirc; Nguyễn L&ecirc;
+            Thoại Ch&acirc;u, Thư k&yacute; khoa).
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
