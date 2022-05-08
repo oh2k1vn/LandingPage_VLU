584f231ffd912955642e9c5bd81ee233b3476462
Hiếu Nguyễn <81700376+oh2k1vn@users.noreply.github.com>
2022-05-08 22:28:04 +0700

    asss

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,13 @@
 import React from "react";
-// import Footer from "./Footer";
+import Footer from "./Footer";
 import Header from "./Header";
-// Hieu
 
 export const Layout = ({ children }) => {
   return (
     <>
       <Header />
       <main>{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
