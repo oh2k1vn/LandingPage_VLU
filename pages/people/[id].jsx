f71f39c014916e0480b53a4b1735058301e4d494
hieunm <hieunm@bytetech.io>
2022-05-13 11:27:21 +0700

    sửa about | fix slider home

--- pages/people/[id].jsx
+++ pages/people/[id].jsx
@@ -1,27 +1,17 @@
-import { Button, Modal, Text } from "@nextui-org/react";
 import Image from "next/image";
-import Link from "../../components/customs/Link";
 import React from "react";
+import { Typography, Stack, Box } from "@mui/material";
+
 import data from "./data.json";
 
 export default function About() {
-  const [dataShow, setDataShow] = React.useState({});
-  const [visible, setVisible] = React.useState(false);
-
-  const closeHandler = () => {
-    setVisible(false);
-  };
-
-  const handlerClickShow = (item) => {
-    setVisible(true);
-    setDataShow(item);
-  };
-
   return (
     <div className="container about">
-      <p className="about_text">Ban Chủ Nhiệm</p>
+      <Typography variant="h2" className="about_text">
+        Ban Chủ Nhiệm
+      </Typography>
       <div className="about_content">
-        {data?.map(item => {
+        {data?.map((item) => {
           return (
             <div className="about_content_item" key={item.id}>
               <Image
@@ -31,92 +21,28 @@ export default function About() {
                 height={320}
                 layout="responsive"
               />
-              <div className="about_content_item_bottom">
-                <p className="about_content_item_bottom-title">
-                {item?.name}
-                </p>
-                <p className="about_content_item_bottom-position">
-                  {item?.position}
-                </p>
-                <p className="about_content_item_bottom-caption">
-                  {item?.desc}
-                </p>
-                <Button onClick={() => handlerClickShow(item)}>Xem thêm</Button>
-              </div>
+
+              <Stack spacing={1}>
+                <Box sx={{ padding: "1rem" }}>
+                  <Typography variant="h5">{item?.name}</Typography>
+                  <Typography
+                    variant="caption"
+                    className="about_content_item_caption"
+                  >
+                    {item?.position}
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ margin: ".2rem 0" }}>
+                    {item?.desc}
+                  </Typography>
+                  <button onClick={() => handlerClickShow(item)}>
+                    Xem thêm
+                  </button>
+                </Box>
+              </Stack>
             </div>
           );
         })}
-        <Modal
-          closeButton
-          aria-labelledby="modal-title"
-          open={visible}
-          onClose={closeHandler}
-        >
-          <Modal.Header>
-            <Text id="modal-title" size={20}>
-              {dataShow?.title}
-            </Text>
-          </Modal.Header>
-          <Modal.Body>{dataShow?.name}</Modal.Body>
-          <Modal.Footer>
-            <Button auto flat color="error" onClick={closeHandler}>
-              Close
-            </Button>
-            <Button auto onClick={closeHandler}>
-              Sign in
-            </Button>
-          </Modal.Footer>
-        </Modal>
       </div>
-      {/* <div className="home">
-        <div className="block">
-          <div className="block_flex">
-            <div className="block_title">
-              <h2 className="block_title_h2">Developer team</h2>
-              <p className="block_title_note">
-                The main responsiblity for the development of products in the
-                Uniworld ecosystem is the Unilab DAO network team.
-                <br />
-                Unilab was established in 2013, with members from many countries
-                around the world (Japan, Canada, Hungary, Singapore, Vietnam...)
-                and operates under the DAO model.
-              </p>
-            </div>
-            <div className="block_img">
-              <Image
-                src="/image/blog1.png"
-                alt=""
-                width={500}
-                height={500}
-                layout="responsive"
-              />
-            </div>
-          </div>
-          <div className="block_flex">
-            <div className="block_title">
-              <h2 className="block_title_h2">Decentralized Community</h2>
-              <p className="block_title_note">
-                Blockchain industry is revolutionary, open source and relies on
-                the community is continuous intellectual contribution.
-                <br />
-                We are well aware of the smallness and limitations of
-                individuals or organizations, so Uniworld is development
-                philosophy is to combine the initiative of the development team
-                and inherit and absorb contributions from the community
-              </p>
-            </div>
-            <div className="block_img">
-              <Image
-                src="/image/blog1.png"
-                alt=""
-                width={500}
-                height={500}
-                layout="responsive"
-              />
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
